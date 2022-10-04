4fa1c14383555ede39ea4b04ab2b5b40c68e235a
郑昊 <@zhenghaoJava>
2022-10-04 18:42:45 +0800

    push commit

--- src/main/java/com/it/Test.java
+++ src/main/java/com/it/Test.java
@@ -5,5 +5,9 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("测试方法");
         System.out.println("master");
+
+        System.out.println("ce测试方法");
+        System.out.println("ce测试单个");
+        System.out.println("pull推送");
     }
 }
